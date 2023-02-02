37f95f9c337f92b3ceef8a00200fb9dfd208d25d
robert <matthewrueter51@gmail.com>
2023-02-02 11:19:14 -0600

    CREATE MultiChoiceExercise sub-component.

--- src/Components/Exercise.js
+++ src/Components/Exercise.js
@@ -4,6 +4,7 @@ export function Exercise (){
 	return (
 		<div className={"exerciseContainer"}>
 			<FlipExercise />
+			<MultiChoiceExercise />
 		</div>
 	)
 }
@@ -17,5 +18,23 @@ const FlipExercise = () =>{
 				<Button action={"Next =>"}></Button>
 			</div>
 		</div>
+	)
+}
+
+const MultiChoiceExercise = () =>{
+	return(
+		<div className={"vocabCardsActivityDisplay"}>
+		<div className={"card"}>TEXT</div>
+		<div className={"buttonBox"}>
+			<Button action={"option"}></Button>
+			<Button action={"option"}></Button>
+			<Button action={"option"}></Button>
+			<Button action={"option"}></Button>
+		</div>
+		<div className={"buttonBox"}>
+			<Button action={"<= Previous"}></Button> 
+			<Button action={"Next =>"}></Button>
+		</div>
+	</div>
 	)
 }
