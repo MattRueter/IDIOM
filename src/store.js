a11e7296a1477524804c8a32bcdc01cf7eb48c58
robert <matthewrueter51@gmail.com>
2023-02-18 13:50:47 -0600

    REDUX tool-kit migration: Swap 'createStore' with 'configureStore'

--- src/store.js
+++ src/store.js
@@ -1,6 +1,10 @@
-import {legacy_createStore} from "redux";
+import {configureStore} from "@reduxjs/toolkit";
 import { setReducer } from "./Reducers/setReducer";
 
-const store = legacy_createStore(setReducer)
+const store = configureStore({
+	reducer:{
+		setReducer:setReducer,
+	}
+})
 
 export default store;
