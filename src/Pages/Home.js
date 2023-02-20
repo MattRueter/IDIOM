cfd9dcbdc5045074d07fbfd5d8d4df16437e8d91
robert <matthewrueter51@gmail.com>
2023-02-20 17:13:52 -0600

    Add handleClick to Home menu buttons which dispatch view type to store.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,9 +2,11 @@ import {Navbar} from '../Components/Navbar.js';
 import {Menu, menuButtons} from '../Components/Menu.js';
 import { DisplayFolders_Sets } from '../Components/FolderAndSetDisplay';
 import { setSelected } from "../Reducers/setReducer";
+import { viewSelected } from "../Reducers/viewReducer"
 import { useSelector, useDispatch } from 'react-redux';
 import { wordLibrary } from "../Data/wordCollection";
 import { filterSets } from '../Utility_functions/utilities';
+
 
 const setA = wordLibrary
 const setB = filterSets(["odd"], "spanish");
@@ -16,8 +18,8 @@ export default function Home() {
   const dispatch = useDispatch();
   console.log(state);
 
-  const handleClick = (exerciseType) => {
-		console.log("clicking from Home.")
+  const handleClick = (view) => {
+		dispatch(viewSelected(view))
 	}
   return (
     <div className={"homePage"}>
