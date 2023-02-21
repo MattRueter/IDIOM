4f551d8282b0a108c4fd1703748780095f49ac51
robert <matthewrueter51@gmail.com>
2023-02-21 10:57:34 -0600

    Flip exericse shows l1 word and buttons cycle back and forth through set.

--- src/Components/Exercise.js
+++ src/Components/Exercise.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { useSelector } from 'react-redux';
 import Button from './Button';
 
 export function Exercise ({currentExercise}){
@@ -18,12 +20,29 @@ export function Exercise ({currentExercise}){
 }
 
 const FlipExercise = () =>{
+	const [ counter, setCounter ] =useState(0)
+	const state = useSelector((state) => state);
+	
+	const currentSet = state.setReducer.set;	
+	const lastWord = currentSet.length -1;
+	const next = "Next";
+	const previous = "Previous";
+
+	const changeCard = (command) =>{
+		if(counter > 0 && counter < lastWord){
+			command === next ? setCounter(counter + 1) : setCounter(counter - 1);
+		}else if(counter === 0) {
+			command === next ? setCounter(counter + 1) : setCounter(lastWord);
+		}else if(counter === lastWord){
+			command === next ? setCounter( 0 ) : setCounter(counter - 1);
+		}
+	}
 	return(
 		<div className={"display"}>
-			<div className={"card"}>TEXT</div>
+			<div className={"card"}>{currentSet[counter].l1}</div>
 			<div className={"buttonBox"}>
-				<Button buttonName={"<= Previous"}></Button> 
-				<Button buttonName={"Next =>"}></Button>
+				<Button handleClick={changeCard} buttonName={previous}></Button> 
+				<Button handleClick={changeCard} buttonName={next}></Button>
 			</div>
 		</div>
 	)
