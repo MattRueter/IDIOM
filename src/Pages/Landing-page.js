b0db3b3a5972da70ab198d02eb90a43ece3cb7e7
robert <matthewrueter51@gmail.com>
2023-02-24 15:15:05 -0600

    Landing page responsiveness and tweaking responsiveness elsewhere.

--- src/Pages/Landing-page.js
+++ src/Pages/Landing-page.js
@@ -1,31 +1,11 @@
 import LoginForm from '../Components/LoginForm.js';
 import {Footer} from '../Components/Footer.js';
 
-const mainStyles = {
-	display: "flex",
-	justifyContent: "space-evenly",
-	alignItems: "center",
-	width: "80%",
-	height: "100%",
-	marginLeft: "5%",
-	padding: "1%"
-}
-const largeLogo = {
-	backgroundColor: "#22187e",
-	color: "white",
-	display: "flex",
-	height: "75%",
-	width: "60%",
-	flexDirection: "column",
-	justifyContent: "center",
-	alignItems: "center",
-	fontSize: "80px"
-}
 export default function LandingPage() {
 	return (
 		<div className={"landingPage"}>
-			<main style={mainStyles}>
-				<div style={largeLogo}>idiom</div>
+			<main className={"LandingPageMainContainer"}>
+				<div className={"LandingPageLargeLogo"}>idiom</div>
 				<LoginForm />
 			</main>
 			<Footer />
