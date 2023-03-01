502c5a21220d3e09958a6011193551df1d4281a4
robert <matthewrueter51@gmail.com>
2023-03-01 11:17:56 -0600

    PORT temporarily defined directly in Server.js. Being processed in config.env file  Was causing connecting issue. Simple routing added to test server and MongoDB connectiion/ querying.

--- server/server.js
+++ server/server.js
@@ -1,13 +1,27 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const app = express();
 require("dotenv").config( { path: "./config.env"} );
-const port = process.env.PORT || 5000;
-app.use(cors());
-app.use(express.json());
-app.use(require("./routes/words"));
-//get driver connection
 const dbo = require("./db/conn");
+
+//const port = process.env.PORT || 5000;
+const port = 5000;
+
+
+app.use(cors());
+//app.use(express.json());
+app.use(require("./routes/testRoutes"));
+
+app.get("/", function(req,res,next){
+	res.send("listening clearly now?")
+});
+
+
+app.get("/words", function(req,res,next) {
+	const currentWords = dbo.getMyWords()
+	res.send(currentWords)
+});
+
 
 app.listen(port, () => {
 	//perform a database connection when server starts
