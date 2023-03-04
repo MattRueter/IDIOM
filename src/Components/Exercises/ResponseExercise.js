9c27ad0f9a563f0ca5fd263abdb1e1b114ac790b
robert <matthewrueter51@gmail.com>
2023-03-03 19:54:22 -0600

    Add checking answer logic and correct/incorrect message.

--- src/Components/Exercises/ResponseExercise.js
+++ src/Components/Exercises/ResponseExercise.js
@@ -1,7 +1,7 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import { counterReset, counterIncreased  } from "../../Reducers/counterReducer"
-import { currentWordChanged, optionsUpdated } from "../../Reducers/exerciseReducer";
+import { currentWordChanged } from "../../Reducers/exerciseReducer";
 import Button from '../Button';
 import { CounterDisplay } from "../CounterDisplay";
 
@@ -9,14 +9,29 @@ export const ResponseExercise = () =>{
 		const state = useSelector((state) =>state);
 		const dispatch = useDispatch();
 		const currentSet  = state.setReducer.set ? state.setReducer.set : [{l1:"choose a set.", l2:"choose a set."}];
+		const toggledLanguage =state.exerciseReducer.toggledLanguage;
 		const currentWord = state.exerciseReducer.currentWord.word;
 		const currentIndex = state.counterReducer.counter;
+		const correctResponse = state.exerciseReducer.currentWord.word[1];
 		const lastWordIndex = currentSet.length-1;
-
+		const input = document.getElementById("input")
+		const [ response, setResponse] = useState("");
+		useEffect(() => {
+			dispatch(currentWordChanged(currentSet[currentIndex]));		
+		},[toggledLanguage, currentIndex]);
+		
 		const checkAnswer = () =>{	
+			response === correctResponse ? 
+				alert(`correct!`) : 
+				alert(`wrong: ${correctResponse}`);
+
+			input.value = "";
 			nextCard() 
 		}
-
+	const updateResponse = (e) => {
+		const newResponse =e.target.value;
+		setResponse(newResponse)
+	}
 		const nextCard = () =>{
 			if(lastWordIndex > 0){
 				if(currentIndex < lastWordIndex){
@@ -30,7 +45,7 @@ export const ResponseExercise = () =>{
 	return(
 		<div className={"display"}>
 			<div className={"card"}>{currentWord[0]}</div>
-			<input className={"exerciseInput"} placeholder={"write your response"}></input>
+			<input id={"input"}className={"exerciseInput"} placeholder={"write your response"} onChange={(e)=>updateResponse(e)}></input>
 			<CounterDisplay />
 			<div className={"buttonBox"}>
 				<Button className={"exerciseButton"} handleClick={checkAnswer}buttonName={"Check"}></Button>
