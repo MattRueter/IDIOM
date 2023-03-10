71001f95417b5a0e65be3a0e3586bdf5445c9415
robert <matthewrueter51@gmail.com>
2023-03-10 16:23:57 -0600

    landing page large image svg implemented.

--- src/Pages/Landing-page.js
+++ src/Pages/Landing-page.js
@@ -5,10 +5,499 @@ export default function LandingPage() {
 	return (
 		<div className={"landingPage"}>
 			<main className={"LandingPageMainContainer"}>
-				<div className={"LandingPageLargeLogo"}>idiom</div>
+				
+					<LandingPageIcon />
+		
 				<LoginForm />
 			</main>
 			<Footer />
 		</div>
 	)
+}
+
+
+function LandingPageIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="90%"
+      height="90%"
+      version="1.1"
+      viewBox="0 0 666.991 469.411"
+    >
+      <defs>
+        <path d="M1297.319 142.693H1400.772V223.55200000000002H1297.319z"></path>
+        <path d="M1313.966 145.071H1401.9599999999998V222.363H1313.966z"></path>
+        <path d="M1312.777 142.693H1400.771V225.931H1312.777z"></path>
+        <path d="M1302.075 145.071H1405.528V223.552H1302.075z"></path>
+        <path d="M529.154 463.753H632.607V541.045H529.154z"></path>
+        <path d="M1845.498 1445.958H2283.09V1669.511H1845.498z"></path>
+        <path d="M746.761 214.04H1132.033V380.515H746.761z"></path>
+        <path d="M761.03 156.963H1155.8139999999999V413.81100000000004H761.03z"></path>
+        <path
+          id="rect13023"
+          d="M-979.827 1992.948H-190.25800000000004V2459.079H-979.827z"
+        ></path>
+      </defs>
+      <g transform="translate(-2.517 42.788)">
+        <path
+          fill="rgb(9, 1, 82)"
+          fillOpacity="1"
+          strokeWidth="31.607"
+          d="M5.034 -45.305H672.025V424.106H5.034z"
+          paintOrder="markers stroke fill"
+        ></path>
+        <path
+          fill="#000"
+          fillOpacity="0.597"
+          fillRule="evenodd"
+          stroke="#000"
+          strokeOpacity="1"
+          strokeWidth="5.761"
+          d="M293.977 61.05l58.902 54.087"
+          paintOrder="stroke markers fill"
+        ></path>
+        <path
+          fill="#000"
+          fillOpacity="0.597"
+          fillRule="evenodd"
+          stroke="#000"
+          strokeOpacity="1"
+          strokeWidth="4.355"
+          d="M511.415 185.736l-62.614 35.015"
+          paintOrder="stroke markers fill"
+        ></path>
+        <path
+          fill="#000"
+          fillOpacity="0.597"
+          fillRule="evenodd"
+          stroke="#000"
+          strokeOpacity="1"
+          strokeWidth="4.828"
+          d="M235.588 276.45l-63.43 52.427"
+          paintOrder="stroke markers fill"
+        ></path>
+        <path
+          fill="#000"
+          fillOpacity="0.597"
+          fillRule="evenodd"
+          stroke="#000"
+          strokeOpacity="1"
+          strokeWidth="2.657"
+          d="M199.764 47.206L168.969 79.9"
+          paintOrder="stroke markers fill"
+        ></path>
+        <path
+          fill="#000"
+          fillOpacity="0.597"
+          fillRule="evenodd"
+          stroke="#000"
+          strokeOpacity="1"
+          strokeWidth="2.447"
+          d="M346.449 10.012l-32.198 26.54"
+          paintOrder="stroke markers fill"
+        ></path>
+        <path
+          fill="#000"
+          fillOpacity="0.597"
+          fillRule="evenodd"
+          stroke="#000"
+          strokeOpacity="1"
+          strokeWidth="6.557"
+          d="M493.057 315.792l-68.686-40.101"
+          paintOrder="stroke markers fill"
+        ></path>
+        <path
+          fill="#000"
+          fillOpacity="0.597"
+          fillRule="evenodd"
+          stroke="#000"
+          strokeOpacity="1"
+          strokeWidth="2.966"
+          d="M191.322 16.652l-39.08-14.427"
+          paintOrder="stroke markers fill"
+        ></path>
+      </g>
+      <g fillOpacity="1" transform="translate(386.352 177.445)">
+        <ellipse
+          cx="-56.002"
+          cy="77.396"
+          fill="#fc3dbd"
+          strokeWidth="116.518"
+          paintOrder="markers stroke fill"
+          rx="127.735"
+          ry="102.566"
+        ></ellipse>
+        <ellipse
+          cx="-135.915"
+          cy="-100.678"
+          fill="#c467f6"
+          strokeWidth="31.065"
+          paintOrder="markers stroke fill"
+          rx="67.958"
+          ry="51.597"
+        ></ellipse>
+        <ellipse
+          cx="-232.189"
+          cy="-45.934"
+          fill="#ff0101"
+          strokeWidth="9.573"
+          paintOrder="markers stroke fill"
+          rx="18.248"
+          ry="18.248"
+        ></ellipse>
+        <ellipse
+          cx="-249.178"
+          cy="-134.657"
+          fill="#ff0101"
+          strokeWidth="9.573"
+          paintOrder="markers stroke fill"
+          rx="18.248"
+          ry="18.248"
+        ></ellipse>
+        <ellipse
+          cx="-28.945"
+          cy="-128.364"
+          fill="#ff0101"
+          strokeWidth="9.573"
+          paintOrder="markers stroke fill"
+          rx="18.248"
+          ry="18.248"
+        ></ellipse>
+        <ellipse
+          cx="183.737"
+          cy="21.394"
+          fill="#c467f6"
+          strokeWidth="31.065"
+          paintOrder="markers stroke fill"
+          rx="67.958"
+          ry="51.597"
+        ></ellipse>
+        <ellipse
+          cx="-275.606"
+          cy="215.199"
+          fill="#c467f6"
+          strokeWidth="31.065"
+          paintOrder="markers stroke fill"
+          rx="67.958"
+          ry="51.597"
+        ></ellipse>
+        <ellipse
+          cx="152.275"
+          cy="215.199"
+          fill="#c467f6"
+          strokeWidth="31.065"
+          paintOrder="markers stroke fill"
+          rx="67.958"
+          ry="51.597"
+        ></ellipse>
+        <text
+          fill="#1e0667"
+          fillRule="nonzero"
+          stroke="#000"
+          strokeDasharray="none"
+          strokeDashoffset="0"
+          strokeOpacity="0"
+          strokeWidth="117.411"
+          display="inline"
+          fontFamily="Gill Sans Ultra Bold Condensed"
+          fontSize="192"
+          fontStretch="normal"
+          fontStyle="normal"
+          fontVariant="normal"
+          fontWeight="normal"
+          paintOrder="stroke markers fill"
+          transform="matrix(.39908 0 0 .60002 243.385 -1195.631)"
+          xmlSpace="preserve"
+          style={{
+            InkscapeFontSpecification:
+              "'Gill Sans Ultra Bold Condensed, Normal'",
+            fontVariantLigatures: "normal",
+            fontVariantCaps: "normal",
+            fontVariantNumeric: "normal",
+            fontVariantEastAsian: "normal",
+            whiteSpace: "pre",
+          }}
+        >
+          <tspan x="-979.826" y="2174.632">
+            <tspan paintOrder="stroke fill markers">idiom</tspan>
+          </tspan>
+        </text>
+        <text
+          xmlSpace="preserve"
+          style={{
+            InkscapeFontSpecification:
+              "'Gill Sans Ultra Bold Condensed, Normal'",
+            fontVariantLigatures: "normal",
+            fontVariantCaps: "normal",
+            fontVariantNumeric: "normal",
+            fontVariantEastAsian: "normal",
+          }}
+          x="-316.83"
+          y="224.224"
+          fill="#1e0667"
+          fillRule="nonzero"
+          stroke="#000"
+          strokeDasharray="none"
+          strokeDashoffset="0"
+          strokeOpacity="0"
+          strokeWidth="31.065"
+          fontFamily="Gill Sans Ultra Bold Condensed"
+          fontSize="50.8"
+          fontStretch="normal"
+          fontStyle="normal"
+          fontVariant="normal"
+          fontWeight="normal"
+          paintOrder="stroke fill markers"
+        >
+          <tspan
+            style={{
+              InkscapeFontSpecification:
+                "'Gill Sans Ultra Bold Condensed, Normal'",
+              fontVariantLigatures: "normal",
+              fontVariantCaps: "normal",
+              fontVariantNumeric: "normal",
+              fontVariantEastAsian: "normal",
+            }}
+            x="-316.83"
+            y="224.224"
+            strokeWidth="31.065"
+            fontFamily="Gill Sans Ultra Bold Condensed"
+            fontSize="28.222"
+            fontStretch="normal"
+            fontStyle="normal"
+            fontVariant="normal"
+            fontWeight="normal"
+          >
+            french
+          </tspan>
+        </text>
+        <text
+          xmlSpace="preserve"
+          style={{
+            InkscapeFontSpecification:
+              "'Gill Sans Ultra Bold Condensed, Normal'",
+            fontVariantLigatures: "normal",
+            fontVariantCaps: "normal",
+            fontVariantNumeric: "normal",
+            fontVariantEastAsian: "normal",
+          }}
+          x="142.513"
+          y="27.75"
+          fill="#1e0667"
+          fillRule="nonzero"
+          stroke="#000"
+          strokeDasharray="none"
+          strokeDashoffset="0"
+          strokeOpacity="0"
+          strokeWidth="31.065"
+          fontFamily="Gill Sans Ultra Bold Condensed"
+          fontSize="28.222"
+          fontStretch="normal"
+          fontStyle="normal"
+          fontVariant="normal"
+          fontWeight="normal"
+          paintOrder="stroke fill markers"
+        >
+          <tspan x="142.513" y="27.75" strokeWidth="31.065">
+            korean
+          </tspan>
+        </text>
+        <text
+          xmlSpace="preserve"
+          style={{
+            InkscapeFontSpecification:
+              "'Gill Sans Ultra Bold Condensed, Normal'",
+            fontVariantLigatures: "normal",
+            fontVariantCaps: "normal",
+            fontVariantNumeric: "normal",
+            fontVariantEastAsian: "normal",
+          }}
+          x="-183.953"
+          y="-93.864"
+          fill="#1e0667"
+          fillRule="nonzero"
+          stroke="#000"
+          strokeDasharray="none"
+          strokeDashoffset="0"
+          strokeOpacity="0"
+          strokeWidth="31.065"
+          fontFamily="Gill Sans Ultra Bold Condensed"
+          fontSize="28.222"
+          fontStretch="normal"
+          fontStyle="normal"
+          fontVariant="normal"
+          fontWeight="normal"
+          paintOrder="stroke fill markers"
+        >
+          <tspan x="-183.953" y="-93.864" strokeWidth="31.065">
+            spanish
+          </tspan>
+        </text>
+        <text
+          xmlSpace="preserve"
+          style={{
+            InkscapeFontSpecification:
+              "'Gill Sans Ultra Bold Condensed, Normal'",
+            fontVariantLigatures: "normal",
+            fontVariantCaps: "normal",
+            fontVariantNumeric: "normal",
+            fontVariantEastAsian: "normal",
+          }}
+          x="108.534"
+          y="222.597"
+          fill="#1e0667"
+          fillRule="nonzero"
+          stroke="#000"
+          strokeDasharray="none"
+          strokeDashoffset="0"
+          strokeOpacity="0"
+          strokeWidth="31.065"
+          fontFamily="Gill Sans Ultra Bold Condensed"
+          fontSize="28.222"
+          fontStretch="normal"
+          fontStyle="normal"
+          fontVariant="normal"
+          fontWeight="normal"
+          paintOrder="stroke fill markers"
+        >
+          <tspan x="108.534" y="222.597" strokeWidth="31.065">
+            catalan
+          </tspan>
+        </text>
+        <text
+          xmlSpace="preserve"
+          style={{
+            InkscapeFontSpecification:
+              "'Gill Sans Ultra Bold Condensed, Normal'",
+            fontVariantLigatures: "normal",
+            fontVariantCaps: "normal",
+            fontVariantNumeric: "normal",
+            fontVariantEastAsian: "normal",
+          }}
+          x="-265.829"
+          y="-130.606"
+          fill="#1e0667"
+          fillRule="nonzero"
+          stroke="#000"
+          strokeDasharray="none"
+          strokeDashoffset="0"
+          strokeOpacity="0"
+          strokeWidth="25.276"
+          fontFamily="Gill Sans Ultra Bold Condensed"
+          fontSize="22.963"
+          fontStretch="normal"
+          fontStyle="normal"
+          fontVariant="normal"
+          fontWeight="normal"
+          paintOrder="stroke fill markers"
+          transform="scale(.99462 1.0054)"
+        >
+          <tspan
+            style={{
+              InkscapeFontSpecification:
+                "'Gill Sans Ultra Bold Condensed, Normal'",
+              fontVariantLigatures: "normal",
+              fontVariantCaps: "normal",
+              fontVariantNumeric: "normal",
+              fontVariantEastAsian: "normal",
+            }}
+            x="-265.829"
+            y="-130.606"
+            strokeWidth="25.276"
+            fontFamily="Gill Sans Ultra Bold Condensed"
+            fontSize="16.075"
+            fontStretch="normal"
+            fontStyle="normal"
+            fontVariant="normal"
+            fontWeight="normal"
+          >
+            verb
+          </tspan>
+        </text>
+        <text
+          xmlSpace="preserve"
+          style={{
+            InkscapeFontSpecification:
+              "'Gill Sans Ultra Bold Condensed, Normal'",
+            fontVariantLigatures: "normal",
+            fontVariantCaps: "normal",
+            fontVariantNumeric: "normal",
+            fontVariantEastAsian: "normal",
+          }}
+          x="-299.026"
+          y="-32.263"
+          fill="#1e0667"
+          fillRule="nonzero"
+          stroke="#000"
+          strokeDasharray="none"
+          strokeDashoffset="0"
+          strokeOpacity="0"
+          strokeWidth="26.381"
+          fontFamily="Gill Sans Ultra Bold Condensed"
+          fontSize="16.777"
+          fontStretch="normal"
+          fontStyle="normal"
+          fontVariant="normal"
+          fontWeight="normal"
+          paintOrder="stroke fill markers"
+          transform="scale(.82989 1.20499)"
+        >
+          <tspan x="-299.026" y="-32.263" strokeWidth="26.381">
+            noun
+          </tspan>
+        </text>
+        <text
+          xmlSpace="preserve"
+          style={{
+            InkscapeFontSpecification:
+              "'Gill Sans Ultra Bold Condensed, Normal'",
+            fontVariantLigatures: "normal",
+            fontVariantCaps: "normal",
+            fontVariantNumeric: "normal",
+            fontVariantEastAsian: "normal",
+          }}
+          x="-49.476"
+          y="-113.08"
+          fill="#1e0667"
+          fillRule="nonzero"
+          stroke="#000"
+          strokeDasharray="none"
+          strokeDashoffset="0"
+          strokeOpacity="0"
+          strokeWidth="25.626"
+          fontFamily="Gill Sans Ultra Bold Condensed"
+          fontSize="16.297"
+          fontStretch="normal"
+          fontStyle="normal"
+          fontVariant="normal"
+          fontWeight="normal"
+          paintOrder="stroke fill markers"
+          transform="scale(.89882 1.11257)"
+        >
+          <tspan
+            style={{
+              InkscapeFontSpecification:
+                "'Gill Sans Ultra Bold Condensed, Normal'",
+              fontVariantLigatures: "normal",
+              fontVariantCaps: "normal",
+              fontVariantNumeric: "normal",
+              fontVariantEastAsian: "normal",
+            }}
+            x="-49.476"
+            y="-113.08"
+            strokeWidth="25.626"
+            fontFamily="Gill Sans Ultra Bold Condensed"
+            fontSize="9.312"
+            fontStretch="normal"
+            fontStyle="normal"
+            fontVariant="normal"
+            fontWeight="normal"
+          >
+            adjective
+          </tspan>
+        </text>
+      </g>
+    </svg>
+  );
 }
