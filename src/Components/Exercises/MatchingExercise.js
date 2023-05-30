89f9d0d4190cf81a4b9907c4a5ec1f89f1e0535c
robert <matthewrueter51@gmail.com>
2023-05-30 14:35:13 -0500

    add card selecting function.

--- src/Components/Exercises/MatchingExercise.js
+++ src/Components/Exercises/MatchingExercise.js
@@ -1,15 +1,36 @@
+import { useState } from "react";
 import { useSelector } from 'react-redux';
 import { shuffleArray } from "../../Utility_functions/utilities";
 
 export const MatchingExercise = () =>{
 	const state = useSelector((state) => state);
 	const currentSet = state.setReducer.set;
+	const [selectedChoices, setSelectedChoices] = useState({l1:"", l2:""})
+
+//clicking events ------------------------------------------
+	const selectL1card =(card)=>{
+		const word = card.target.textContent
+		setSelectedChoices(prev =>({
+			...prev,
+			l1:word
+		}));
+		console.log(selectedChoices)
+	}
+	const selectL2card =(card)=>{
+		const word = card.target.textContent
+		setSelectedChoices(prev =>({
+			...prev,
+			l2:word
+		}));
+		console.log(selectedChoices)
+	}
+//-----------------------------------------------------------	
 	//make and shuffle L1 cards
 	let L_oneCards = currentSet.map(item => item);
 	L_oneCards = shuffleArray(L_oneCards);
 	L_oneCards = L_oneCards.map((item,index) =>{
 		return(
-			<Card word={item.l1} key={index} />
+			<Card word={item.l1} key={index} handleClick={selectL1card}/>
 		)
 	});
 	//make and shuffle L2 cards
@@ -17,7 +38,7 @@ export const MatchingExercise = () =>{
 	L_twoCards = shuffleArray(L_twoCards);
 	L_twoCards = L_twoCards.map((item,index) =>{
 		return(
-			<Card word={item.l2} key={index} />
+			<Card word={item.l2} key={index} handleClick={selectL2card}/>
 		)
 	});
 	return(
@@ -31,8 +52,8 @@ export const MatchingExercise = () =>{
 		</div>
 	)
 }
-const Card = ({word}) =>{
+const Card = ({word, handleClick}) =>{
 	return (
-		<div className={"smallCard"}>{word}</div>
+		<div className={"smallCard"} onClick={handleClick}>{word}</div>
 	)
 }
