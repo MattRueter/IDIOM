6e9a8c32c6feb6f91903b41035caa6ec18bcf4de
robert <matthewrueter51@gmail.com>
2023-05-30 12:11:07 -0500

    create MatchingExercise file for component.

---
+++ src/Components/Exercises/MatchingExercise.js
@@ -0,0 +1,25 @@
+export const MatchingExercise = () =>{
+	return(
+		<div className={"displayROW"}>
+			<div className={"CardColumn"}>
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+			</div>
+			<div className={"CardColumn"}>
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+			</div>
+		</div>
+	)
+}
+const Card = () =>{
+	return (
+		<div className={"smallCard"}>TEXT</div>
+	)
+}
