9de54ac975a537234da24757d00bd8c1cc874f75
robert <matthewrueter51@gmail.com>
2023-05-30 12:37:39 -0500

    Shuffle both L1 and L2 cards before displaying in MatchingExercise.

--- src/Components/Exercises/MatchingExercise.js
+++ src/Components/Exercises/MatchingExercise.js
@@ -1,14 +1,21 @@
 import { useSelector } from 'react-redux';
+import { shuffleArray } from "../../Utility_functions/utilities";
 
 export const MatchingExercise = () =>{
 	const state = useSelector((state) => state);
 	const currentSet = state.setReducer.set;
-	const L_oneCards =currentSet.map((item,index) =>{
+	//make and shuffle L1 cards
+	let L_oneCards = currentSet.map(item => item);
+	L_oneCards = shuffleArray(L_oneCards);
+	L_oneCards = L_oneCards.map((item,index) =>{
 		return(
 			<Card word={item.l1} key={index} />
 		)
 	});
-	const L_twoCards = currentSet.map((item,index) =>{
+	//make and shuffle L2 cards
+	let L_twoCards = currentSet.map(item => item);
+	L_twoCards = shuffleArray(L_twoCards);
+	L_twoCards = L_twoCards.map((item,index) =>{
 		return(
 			<Card word={item.l2} key={index} />
 		)
