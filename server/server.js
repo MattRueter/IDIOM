afa183a54d0cc034c32cf91f7ee9114208e8a659
robert <matthewrueter51@gmail.com>
2023-08-14 11:31:52 -0500

    erase old boilerplate.

--- server/server.js
+++ server/server.js
@@ -1,30 +1,26 @@
 const express = require("express");
-const cors = require("cors");
+//const cors = require("cors");
 const app = express();
-require("dotenv").config( { path: "./config.env"} );
-const dbo = require("./db/conn");
+
 
 //const port = process.env.PORT || 5000;
 const port = 5000;
 
 
-app.use(cors());
-//app.use(express.json());
-app.use(require("./routes/testRoutes"));
+//app.use(cors());
+app.use(express.json());
 
 app.get("/", function(req,res,next){
-	res.send("listening clearly now?")
+	res.send("listening and responding.")
 });
 
 
 app.get("/words", function(req,res,next) {
-	const currentWords = dbo.getMyWords()
-	res.send(currentWords)
+
+	res.send("The library would be sent here. Remember to check authentication.")
 });
 
 
 app.listen(port, () => {
-	//perform a database connection when server starts
-	dbo.connectToServer();
 	console.log(`Server is running on port: ${port}`);
 });
