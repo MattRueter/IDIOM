53d7ac3511299eab63290fd2e77b1ad0ae5831c5
robert <matthewrueter51@gmail.com>
2023-08-14 12:48:58 -0500

    add connect to DB logic.

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,1 +1,23 @@
 // connect to Idiom DB and pass that connection on to whole app.
+require("../loadEnvironment.js");
+const { MongoClient } = require("mongodb");
+const connectionString = process.env.ATLAS_URI;
+console.log("string below")
+console.log(connectionString);
+const client = new MongoClient(connectionString);
+
+//establish connection or log error.
+let conn;
+let db;
+async function connectToDb () {
+	try{
+		conn = await client.connect();
+		db = conn.db( "idiomDB" );
+
+	}catch(e){
+		console.log(e);
+	}
+}
+
+connectToDb()
+module.exports = db;
