b24870a1f39fd20bacb951ddef3be2baba3adb2e
Matt <84277551+MattRueter@users.noreply.github.com>
2023-12-09 17:56:29 -0600

    update styles and layout of landing page & add logo as link to navbar.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,15 +1,19 @@
+import logo from '../Images/logo.svg';
+
 export const footer ={
 	display: "flex",
 	justifyContent: "flex-start",
 	alignItems: "flex-start",
 	backgroundColor: "#090152",
-	height: "12%",
+
 	marginTop:"2%"
 }
 export function Footer ({content}){
 	return (
 		<nav style={footer}>
-			<div className={"smLogo"}></div>
+			<div style={{width:"20%"}}>
+				<img src={logo} />
+			</div>
 		</nav>
 	)
 }
