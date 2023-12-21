24a9745867dd2d6bfe9c007f971f3a2473b228b3
Matt <84277551+MattRueter@users.noreply.github.com>
2023-12-21 10:21:00 -0600

    style 404 page.

--- idiom/src/app/not-found.js
+++ idiom/src/app/not-found.js
@@ -1,18 +1,25 @@
 import Image from "next/image"
 import logo from '../../public/logo.svg'
+import errorImg from '../../public/404.svg'
 
 export default function notFound () {
     return(
-        <main className="flex flex-col justify-center items-center h-screen bg-pink-300 text-white">
-            <div className="flex flex-col items-center justify-between h-1/4">
+        <main className="flex flex-col items-center h-screen bg-white text-black p-6">
+            <div className="flex flex-col items-center justify-between">
                 <Image
                     src={logo}
                     alt="logo"
                     width={100}
                     height={100}
+                />
+                <Image
+                    src={errorImg}
+                    alt="not found cartoon"
+                    width={500}
+                    height={500}
                     priority
                 />
-                <h1>404 Not found</h1>
+                <h1>Error. Page not found</h1>
             </div>
         </main>
     )
